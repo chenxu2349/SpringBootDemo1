49503fac28cf793d5e14c06a01280fa841d5e536
chenxu2349 <chenxu_2349@qq.com>
2023-11-15 18:58:46 +0800

    Initial commit

---
+++ src/main/java/com/example/springbootdemo1/controller/Hello.java
@@ -0,0 +1,20 @@
+package com.example.springbootdemo1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @ClassName Hello
+ * @Description
+ * @Author chenxu
+ * @Date 2023/11/15 18:38
+ **/
+@Controller
+public class Hello {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "hello jenkins";
+    }
+}
